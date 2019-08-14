637dd8f86aa662d4b66d2be5642689a90f72ba8f
Iris Liu <53001901+irises0605@users.noreply.github.com>
2019-08-14 15:09:04 +1000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,9 @@
+/**
+ * Challenge 3
+ * Author: Bingya Liu
+ * Date: 06/15/2019
+ **/
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,32 +13,39 @@ import java.util.*;
 import java.lang.Object;
 
 public class Main {
+    
 
 	public static class Asset {
+        /** the Asset class **/
 		String name;
 		String type;
 		int shares;
 		int price;
 		double accruedInterest;
+        
 		
 		public Asset( String name, String type, int shares, int price, double accruedInterest){
+            /** constructor that takes 5 parameters, given in the challenge**/
 			this.name = name;
 			this.type = type;
 			this.shares = shares;
 			this.price = price;
 			this.accruedInterest = accruedInterest;
 		}
+        
 		
 		public double getMarketValue(){
+            /** method that gets the market value of the asset based on type **/
 			if (this.type.equals("STOCK")) {
 				return this.shares*this.price;
 			}
 			else {
-				//System.out.println(this.shares*(this.price+this.accruedInterest)*0.01);
 				return this.shares*(this.price+this.accruedInterest)*0.01;
 			}
 		}
+        
 		
+        //Getters
 		public String getName(){
 			return this.name;
 		}
@@ -48,22 +61,26 @@ public class Main {
         public double getAccruedInterest(){
             return this.accruedInterest;
         }
+      
         
-	}
+	}// End of Asset class
+    
+    
   
 	public static class CollectionOfAssets {
+        /** the Collection of Assets class **/
 		ArrayList<Asset> assets;
-		//HashMap<String,Double> pairs;
 		
 		
 		public CollectionOfAssets(String[] items){
+            /** constructor **/
 			this.assets = new ArrayList<Asset>();
-			//this.pairs = new HashMap<String,Double>();
 			ManageAssets(items);
 		}
+        
 		
 		private void ManageAssets(String[] items){
-            
+            /** method that create new Asset objects using input string[] **/
 			for(int i=0; i<items.length; i++){
                 String[] item = items[i].split(",");
 				String name = item[0];
@@ -73,43 +90,53 @@ public class Main {
 				Double accruedInterest = Double.parseDouble(item[4]);
 				Asset newAsset = new Asset( name, type, shares, price, accruedInterest);
 				assets.add(newAsset);
-				//pairs.put(name, price);
 			}
 		}
+        
 		
 		public ArrayList<Asset> getAssets(){
+            /** method that returns an ArrayList of assets **/
 			return this.assets;
 		}
-// 		
-// 		public double getPrice( String name){
-// 			return this.pairs.get(name);
-// 		}
+        
 		
 		public double TotalMarketValue(){
+            /** method that returns total market value of the collection of assets **/
 			double sum = 0.0;
 			for(int i=0; i<this.assets.size(); i++){
 				sum = sum + assets.get(i).getMarketValue();
 			}
 			return sum;
 		}
-
-	}
+      
+	}// End of CollectionOfAssets class
+    
+    
 	
 	public static class Trade {
-	
+        /** the Trade class **/
 		HashMap<String,Double> map;
 		double benchmarkTMV, portfolioTMV;
+        
 		
 		public Trade( CollectionOfAssets portfolio, CollectionOfAssets benchmark){
+            /** constructor that takes two CollectionOfAssets Objects, one portfolio and one benchmark **/
 			this.map = new HashMap<String,Double>();
 			this.benchmarkTMV = benchmark.TotalMarketValue();
 			this.portfolioTMV = portfolio.TotalMarketValue();
+            
+            //map stores <Name of Assets, benchmark Market percentage> key-value pairs
 			for(Asset asset: benchmark.getAssets()){
 				this.map.put(asset.getName(), asset.getMarketValue()/benchmarkTMV);
 			}
+          
+            
 			for(Asset asset: portfolio.getAssets()){
+                //map stores <Name of Assets, differenence in benchmark&portfolio Market percentage> key-value pairs
 				if (this.map.containsKey(asset.getName())) {
                   this.map.put(asset.getName(), map.get(asset.getName())-asset.getMarketValue()/portfolioTMV);
+                  
+                  //map stores <Name of Assets, number of shares> key-value pairs based on the types of assets
                   if (asset.getType().equals("STOCK")){
 				      this.map.put(asset.getName(), map.get(asset.getName())/asset.getPrice());
                   }
@@ -120,12 +147,14 @@ public class Main {
 				}
 			}
 		}
+        
 		
 		public void Results(){
-		
+            /** method that handles the correct printout format based on postive/neagtive **/
 			Collection<String> keys = this.map.keySet();
     		String[] keySet = keys.toArray(new String[keys.size()]);
-    		Arrays.sort(keySet);
+    		Arrays.sort(keySet); //alphabetic order
+          
     		for(String key: keySet){
     			if(map.get(key)>0){
     			System.out.println("BUY"+","+key+","+Math.round(map.get(key)));
@@ -137,13 +166,18 @@ public class Main {
     			}
     		}
 		}
-	}
+    
+	}// End of Trade class
+    
 	
 	public static void main(String[] args) throws IOException {
+        /** the main method **/
     	InputStreamReader reader = new InputStreamReader(System.in, StandardCharsets.UTF_8);
     	BufferedReader in = new BufferedReader(reader);
     	String line;
+        
     	while ((line = in.readLine()) != null) {
+          
       	    String[] string = line.split(":");
       	    String[] portfolioItems = string[0].split("\\|");
       	    String[] benchmarkItems = string[1].split("\\|");
@@ -151,6 +185,7 @@ public class Main {
 		    CollectionOfAssets benchmark = new CollectionOfAssets(benchmarkItems);
 		    Trade trade = new Trade(portfolio, benchmark);
 		    trade.Results();
+        
     	}
     }
 }
